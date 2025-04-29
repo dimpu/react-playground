4986d8983289bb4fbf7df9bf1ee81a93359bf7de
dimpu.buddha <dimpu.buddha@bytedance.com>
2025-04-28 17:15:03 -0700

    feat(): Tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,53 @@
 import './App.css';
-import { LiveProvider, LiveEditor, LivePreview } from "react-live";
-
+import { LiveProvider, LiveEditor, LivePreview, LiveError } from 'react-live';
 
 const code = `
-<h3 style={{
-  background: 'darkslateblue',
-  color: 'white',
-  padding: 8,
-  borderRadius: 4
-}}>
-  Hello World! 👋
-</h3>
+type Props = {
+  label: string;
+}
+const Counter = (props: Props) => {
+  const [count, setCount] =
+    React.useState<number>(0)
+  return (
+    <div>
+      <h3 style={{
+        background: 'darkslateblue',
+        color: 'white',
+        padding: 8,
+        borderRadius: 4
+      }}>
+        {props.label}: {count} 🧮
+      </h3>
+      <button
+        style={{
+          background: '#333',
+          color: '#fff',
+          borderRadius: 4,
+          padding: 8,
+          margin: 4
+        }}
+        onClick={() =>
+          setCount(c => c + 1)
+        }>
+        Increment
+      </button>
+    </div>
+  )
+}
+render(<Counter label="Counter" />)
+
 `;
 
 const App = () => {
   return (
-    <div className="content">
-      <LiveProvider code={code}>
-        <div className="grid grid-cols-2 gap-4">
-          <LiveEditor className="font-mono" />
-          <LivePreview />
+    <div className="content h-screen">
+      <LiveProvider code={code} noInline>
+        <div className="flex">
+          <LiveEditor className="font-mono flex-1 h-screen" />
+          <div className="flex flex-col flex-1 h-screen p-4">
+            <LivePreview />
+            <LiveError className="text-red-800 bg-red-100 mt-2" />
+          </div>
         </div>
       </LiveProvider>
     </div>
